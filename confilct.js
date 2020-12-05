8cd623c1912a3338771c780831ed5af797392a12
Erhan Kudretli <erhankudretli@gmail.com>
2020-12-05 12:03:09 +0100

    düzenleyip merge yaptim

--- confilct.js
+++ confilct.js
@@ -1,6 +1,4 @@
 let number=62;
 let divider=2;
-let conflictOrnegi = "Conflict icin branchde eklendi, maine dönüp degisiklik yapildi.";
 let  main = "main branchde eklendi";
 let farkliSatir = "mainde farkli satira bir degisken daha eklendi";  
-// bu aciklama main branchte eklendi. newbranch te iken degistirildi.
