c40e68d91725000fc03560e873cceb153dd42d3a
Erhan Kudretli <erhankudretli@gmail.com>
2020-12-05 12:01:01 +0100

    Degisiklik yaptim

--- confilct.js
+++ confilct.js
@@ -1,5 +1,5 @@
-let number=6;
-let divider=3;
+let number=62;
+let divider=2;
 let conflictOrnegi = "Conflict icin branchde eklendi, maine dönüp degisiklik yapildi.";
 let  main = "main branchde eklendi";
 let farkliSatir = "mainde farkli satira bir degisken daha eklendi";  
